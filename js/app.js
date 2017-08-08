814a6bb38af202f67251a6b0d910dc88a16aa604
zhengwei <zhengwei1949@qq.com>
2017-08-08 23:56:21 +0800

    ad

--- js/app.js
+++ js/app.js
@@ -1,54 +1,33 @@
 (function (angular) {
 	'use strict';
+
 	// Your starting point. Enjoy the ride!
-  
-  // 1.创建模块
-  var app = angular.module('todosApp', [])
+	var myApp = angular.module('myApp',['service']);
+	myApp.controller('myController',['$scope','$location','MyService',function($scope,$location,MyService){
+		//要展示的任务数据
+		$scope.todos = MyService.get();
 
-  // 2.创建控制器
-  app.controller('todosController', ['$scope', function($scope){
-    
-    // 功能1.任务的展示(ng-repeat)
-    // 假设已经得到数据
-    $scope.todos = [
-    {id:1,name:'吃饭',completed:true},
-    {id:2,name:'睡觉',completed:true},
-    {id:3,name:'打豆豆',completed:false},
-    {id:4,name:'学习',completed:true},
-    {id:5,name:'喝水',completed:false},
-    ]
+		//添加任务
+		$scope.newTodo=''  // ng-model
+		$scope.add = function(){
+			// 判断newTodo是否为空，为空则不添加任务
+			if(!$scope.newTodo){
+				return
+			}
 
-    // 功能2.添加任务
-    $scope.newTodo=''  // ng-model
-    $scope.add = function(){
-      // 判断newTodo是否为空，为空则不添加任务
-      if(!$scope.newTodo){
-        return
-      }
+			// 把新任务添加到$scope.todos中去
+			MyService.add($scope.newTodo);
+			
+			// 置空
+			$scope.newTodo=''
+		}
 
-      // 把新任务添加到$scope.todos中去
-      $scope.todos.push({
-        id:Math.random(),
-        name:$scope.newTodo,
-        completed:false
-      })
-      // 置空
-      $scope.newTodo=''
+	//删除任务
+	$scope.remove = function(id){
+		MyService.remove(id);
     }
 
-    // 功能3.删除任务
-    $scope.remove = function(id){
-      // 根据id到数组$scope.todos中查找相应元素，并删除
-      for (var i = 0; i < $scope.todos.length; i++) {
-        var item = $scope.todos[i]
-        if(item.id === id){
-          $scope.todos.splice(i,1) // 删除数据
-          return
-        }
-      }
-    }
-
-    // 功能4：修改任务内容 
+	// 功能4：修改任务内容 
     $scope.isEditingId = -1
     $scope.edit = function(id){
       $scope.isEditingId = id
@@ -57,31 +36,24 @@
     // 只是改变也文本框的编辑状态
     $scope.save = function(){
       $scope.isEditingId = -1
+      MyService.save() 
     }
 
     // 功能5.修改任务状态
+    $scope.statusChange = function(){
+      this.save()
+    }
 
     // 功能6.批量切换任务状态
     $scope.selectAll = false
     $scope.toggleAll = function(){
       // 让$scope.todos中所有数据的completed值等于$scope.selectAll
-      for (var i = 0; i < $scope.todos.length; i++) {
-        var item = $scope.todos[i]
-        item.completed = $scope.selectAll
-      }
+      MyService.toggleAll($scope.selectAll)
     }
 
     // 功能7.显示未完成任务数
     $scope.getActive = function(){
-      var count = 0
-      // 遍历$scope.todos, 找到所有completed属性值为false的数据
-      for (var i = 0; i < $scope.todos.length; i++) {
-        var item = $scope.todos[i]
-        if(!item.completed){
-          count++
-        }
-      }
-      return count
+      return MyService.getActive()
     }
 
     // 功能8.清除所有已完成任务
@@ -101,14 +73,43 @@
       //   }
       // }
 
-      for (var i = $scope.todos.length - 1; i >= 0; i--) {
-        // true(0),false(1),false(2)
-        var item = $scope.todos[i]
-        if(item.completed){
-          $scope.todos.splice(i,1)
-        }
-      }
+      MyService.clearAll()
     }
-  }]) 
 
+    // 功能9. 切换不同状态任务的显示
+    $scope.isCompleted={} // filter过滤器的过滤条件
+
+    // // 显示未完成任务
+    // $scope.active = function(){
+    //   $scope.isCompleted = {completed:false}
+    // }
+
+    // // 显示已完成任务
+    // $scope.completed = function(){
+    //   $scope.isCompleted = {completed: true}
+    // }
+
+    // // 显示所有任务
+    // $scope.all = function(){
+    //   $scope.isCompleted={}
+    // }
+    // location.hash
+
+    //根据url中锚点值去切换不同状态任务的显示
+
+    $scope.loca = $location
+    $scope.$watch('loca.url()',function(now, old){
+      switch(now){
+        case '/active':
+        $scope.isCompleted = {completed:false}
+        break;
+        case '/completed':
+        $scope.isCompleted = {completed:true}
+        break;
+        default:
+        $scope.isCompleted = {}
+      }
+    })
+
+	}]);
 })(angular);
